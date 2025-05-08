763113dd7676b877931e537903b4e1410ff30913
Timofey Labor <timofeylabor@gmail.com>
2025-05-08 07:47:02 +0300

    fix format f32a

--- f32a/sum_of_digits.s
+++ f32a/sum_of_digits.s
@@ -3,14 +3,15 @@
 input_addr:      .word  0x80
 output_addr:     .word  0x84
 divider:         .word  10
-is_positive:     .word  0   \ is_positive=false
+is_positive:     .word  0                  \ is_positive=false
 
     .text
     .org 0x90
 
 _start:
-    @p input_addr a! @ dup a!   \ num -> A
-    lit divider b!          \ divider -> [B]
+    @p input_addr a! @
+    dup a!                   \ num -> A
+    lit divider b!           \ divider -> [B]
 
     -if sum_of_digits
 
@@ -18,12 +19,12 @@ _start:
     if negative
 
 negative:
-    a inv lit 1 + a! \ -A -> A
+    a inv lit 1 + a!         \ -A -> A
 
     sum_of_digits
 
 divide:
-    lit 0 lit 0         \ 0 -> remainder; 0 -> quotient
+    lit 0 lit 0              \ 0 -> remainder; 0 -> quotient
     lit 31 >r
 divide_do:
     +/
@@ -31,8 +32,8 @@ divide_do:
     ;
 
 sum_of_digits:
-    lit 1 !p is_positive  \ is_positive=true
-    lit 0                 \ result -> stack[0]
+    lit 1 !p is_positive     \ is_positive=true
+    lit 0                    \ result -> stack[0]
 
     while
 
@@ -40,13 +41,13 @@ sum_of_digits:
 
     halt
 while:
-    a 
+    a
     if finish
 
     divide
 
-    a!                  \ dividend = num\\10; num\\10 -> A
-    +                   \ remainder = num%10; num%10 -> stack[0]; stack[0] = result
+    a!                       \ dividend = num\\10; num\\10 -> A
+    +                        \ remainder = num%10; num%10 -> stack[0]; stack[0] = result
 
     while
     ;
